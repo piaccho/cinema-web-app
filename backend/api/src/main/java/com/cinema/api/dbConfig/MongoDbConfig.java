0934d683af70513f4ec270b2c9a68ecf1123a6b9
adam147g <adasadas223@wp.pl>
2024-01-14 18:23:18 +0100

    Added users to DB

--- backend/api/src/main/java/com/cinema/api/dbConfig/MongoDbConfig.java
+++ backend/api/src/main/java/com/cinema/api/dbConfig/MongoDbConfig.java
@@ -2,14 +2,20 @@ package com.cinema.api.dbConfig;
 
 import com.cinema.api.Category.CategoryRepository;
 import com.cinema.api.Hall.HallRepository;
+import com.cinema.api.Movie.Movie;
 import com.cinema.api.Movie.MovieRepository;
 import com.cinema.api.Reservation.ReservationRepository;
 import com.cinema.api.Showing.ShowingRepository;
+import com.cinema.api.User.User;
 import com.cinema.api.User.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class MongoDbConfig {
@@ -24,6 +30,8 @@ public class MongoDbConfig {
             MongoTemplate mongoTemplate) {
 
         return args -> {
+
+
             System.out.println("--------");
         };
     }
